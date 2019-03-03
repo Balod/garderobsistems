767792ca4cf31fb38ce7331c0d7b65aab796fd67
ilia.balod <2462802@gmail.com>
2019-03-03 06:37:35 +0500

    Add 3 more sections

--- js/common.js
+++ js/common.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-    $('.pop-g').magnificPopup({type:'image'});
+$(document).ready(function () {
+    function gallery() {
+        var item = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        for (var i = 0; i < item.length; i++) {
+            if (item[i] < 10) {
+                $(".img-" + item[i]).css("background-image", "url(../public_html/img/gallery/0" + item[i] + ".jpg");    
+            } else {
+                $(".img-" + item[i]).css("background-image", "url(../public_html/img/gallery/" + item[i] + ".png");
+            }
+        }
+    }
+    gallery();
+
+    $(".gallery-wrap").magnificPopup({
+        delegate: "a",
+        type: "image",
+        gallery: {
+            enabled: true
+        }
+    });
 })
